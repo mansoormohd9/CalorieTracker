1b78985468634ff0b743c56745571093816e0171
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2021-09-15 21:38:43 +0530

    Setting Up Reports

---
+++ CalorieTracker/CalorieTrackerApi/Dtos/EntryStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalorieTrackerApi.Dtos
+{
+    public class EntryStats
+    {
+        public int FoodEntriesAdded { get; set; }
+        public float CaloriesConsumed { get; set; }
+        public float AverageCaloriesConsumed { get; set; }
+    }
+}
---
+++ CalorieTracker/CalorieTrackerApi/Dtos/ReportDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalorieTrackerApi.Dtos
+{
+    public class ReportDto
+    {
+        public EntryStats LastSevenDays { get; set; }
+        public EntryStats PastWeek { get; set; }
+        public EntryStats CurrentDay { get; set; }
+
+    }
+}
