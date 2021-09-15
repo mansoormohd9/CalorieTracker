1a9dbcfedcf625214a99db6b0955731346019257
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2021-09-15 23:28:32 +0530

    UI Reports Added

--- CalorieTracker/CalorieTracker/Controllers/ReportController.cs
+++ CalorieTracker/CalorieTracker/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CalorieTracker.Authentication;
+using CalorieTrackerApi.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,81 +9,26 @@ using System.Threading.Tasks;
 
 namespace CalorieTracker.Controllers
 {
+    [AdminUiAccessRequired]
     public class ReportController : Controller
     {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
         // GET: ReportController
         public ActionResult Index()
         {
-            return View();
+            return View(_reportService.GetUserReports());
         }
 
         // GET: ReportController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(string userName)
         {
-            return View();
-        }
-
-        // GET: ReportController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ReportController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReportController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ReportController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReportController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ReportController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View(_reportService.GetUserReports(userName)[0]);
         }
     }
 }
