3f72435ce6eed68cc79ee0a1edbde8f44177a3a1
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2021-09-16 12:57:38 +0530

    Adding validation attribute on Calories

--- CalorieTracker/CalorieTrackerApi/Dtos/FoodEntryDto.cs
+++ CalorieTracker/CalorieTrackerApi/Dtos/FoodEntryDto.cs
@@ -11,6 +11,7 @@ namespace CalorieTrackerApi.Dtos
         [Required]
         public string Name { get; set; }
         [Required]
+        [Range(1, 2100, ErrorMessage = "Please enter a value bigger than {1}")]
         public double Calories { get; set; }
         [Required]
         public DateTime Date { get; set; }
