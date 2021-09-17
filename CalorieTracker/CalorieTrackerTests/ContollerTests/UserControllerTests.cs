4b79fa52c90639de74fed4462b9aded909dc7f12
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2021-09-17 12:35:33 +0530

    Adding in UserControllerTests

--- CalorieTracker/CalorieTrackerTests/ContollerTests/UserControllerTests.cs
+++ CalorieTracker/CalorieTrackerTests/ContollerTests/UserControllerTests.cs
@@ -9,22 +9,28 @@ using System.Threading.Tasks;
 namespace CalorieTrackerTests.ContollerTests
 {
     [TestFixture]
-    public class UserControllerTests: ControllerTestBase<CalorieTracker.Startup>
+    public class UserControllerTests: ControllerTestBase
     {
-        private readonly HttpClient _client;
-        private readonly ControllerTestBase<CalorieTracker.Startup> _factory;
+        private HttpClient _client;
+        private ControllerTestBase _factory;
 
-        public UserControllerTests(
-            ControllerTestBase<CalorieTracker.Startup> factory)
+        [OneTimeSetUp]
+        public void OneTimeSetup()
         {
-            _factory = factory;
-            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            _factory = new ControllerTestBase();
+            _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
             {
                 AllowAutoRedirect = false
             });
         }
 
+
         [TestCase("/User")]
+        [TestCase("/User/Index")]
+        [TestCase("/User/Details")]
+        [TestCase("/User/Create")]
+        [TestCase("/User/Edit")]
+        [TestCase("/User/DeleteUser")]
         public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
             // Arrange
@@ -36,6 +42,19 @@ namespace CalorieTrackerTests.ContollerTests
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.AreEqual("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [TestCase("/User/DeleteUserY")]
+        public async Task Get_EndpointsReturnFailureStatus(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.NotFound);
         }
     }
 }
--- CalorieTracker/CalorieTrackerTests/ControllerTestBase.cs
+++ CalorieTracker/CalorieTrackerTests/ControllerTestBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using CalorieTracker;
 using CalorieTrackerApi.Data;
 using CalorieTrackerTests.Helpers;
 using Microsoft.AspNetCore.Hosting;
@@ -10,45 +11,45 @@ using Microsoft.Extensions.Logging;
 
 namespace CalorieTrackerTests
 {
-    public class ControllerTestBase<TStartup> : WebApplicationFactory<IStartup> where TStartup : class
+    public class ControllerTestBase : WebApplicationFactory<Startup>
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType ==
-                        typeof(DbContextOptions<CalorieTrackerDbContext>));
+        //protected override void ConfigureWebHost(IWebHostBuilder builder)
+        //{
+        //    builder.ConfigureServices(services =>
+        //    {
+        //        var descriptor = services.SingleOrDefault(
+        //            d => d.ServiceType ==
+        //                typeof(DbContextOptions<CalorieTrackerDbContext>));
 
-                services.Remove(descriptor);
+        //        services.Remove(descriptor);
 
-                services.AddDbContext<CalorieTrackerDbContext>(options =>
-                {
-                    options.UseInMemoryDatabase("InMemoryDbForTesting");
-                });
+        //        services.AddDbContext<CalorieTrackerDbContext>(options =>
+        //        {
+        //            options.UseInMemoryDatabase("InMemoryDbForTesting");
+        //        });
 
-                var sp = services.BuildServiceProvider();
+        //        var sp = services.BuildServiceProvider();
 
-                using (var scope = sp.CreateScope())
-                {
-                    var scopedServices = scope.ServiceProvider;
-                    var db = scopedServices.GetRequiredService<CalorieTrackerDbContext>();
-                    var logger = scopedServices
-                        .GetRequiredService<ILogger<ControllerTestBase<IStartup>>>();
+        //        using (var scope = sp.CreateScope())
+        //        {
+        //            var scopedServices = scope.ServiceProvider;
+        //            var db = scopedServices.GetRequiredService<CalorieTrackerDbContext>();
+        //            var logger = scopedServices
+        //                .GetRequiredService<ILogger<ControllerTestBase<IStartup>>>();
 
-                    db.Database.EnsureCreated();
+        //            db.Database.EnsureCreated();
 
-                    try
-                    {
-                        Utilities.InitializeDbForTests(db);
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "An error occurred seeding the " +
-                            "database with test messages. Error: {Message}", ex.Message);
-                    }
-                }
-            });
-        }
+        //            try
+        //            {
+        //                Utilities.InitializeDbForTests(db);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                logger.LogError(ex, "An error occurred seeding the " +
+        //                    "database with test messages. Error: {Message}", ex.Message);
+        //            }
+        //        }
+        //    });
+        //}
     }
 }
