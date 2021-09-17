efbd11a312dd5cd187e0f6b73ce4e131085b1797
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2021-09-17 13:27:13 +0530

    Adding in controller tests

---
+++ CalorieTracker/CalorieTrackerApiTests/ControllerTestBase.cs
@@ -0,0 +1,12 @@
+﻿using CalorieTrackerApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalorieTrackerApiTests
+{
+    public class ControllerTestBase: WebApplicationFactory<Startup>
+    {
+    }
+}
